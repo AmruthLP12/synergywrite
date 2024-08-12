012f6d9a5d69a57f6b8ee0b435c655bd9954e445
Amruth L P <134767358+AmruthLP12@users.noreply.github.com>
2024-08-12 11:19:52 +0530

    added clerk for auth

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import { Editor } from "@/components/editor/Editor";
+import Header from "@/components/Header";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import React from "react";
 
 const Document = () => {
   return (
-    <div>Document</div>
-  )
-}
+    <div>
+      <Header>
+        <div className="flex w-fit items-center justify-center">
+          <p className="document-title">This is fake</p>
+        </div>
+        <SignedOut>
+            <SignInButton />
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+      </Header>
+      <Editor />
+    </div>
+  );
+};
 
-export default Document
+export default Document;
